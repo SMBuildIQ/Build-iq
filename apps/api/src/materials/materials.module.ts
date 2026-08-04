b226de9a3288b0c19339a6e30845bf1b476b9c58
Clint Lamanen <clintlamanen@icloud.com>
2026-08-04 16:22:09 +0000

    Fix MaterialsModule AuthModule import so Nest API can boot.

--- apps/api/src/materials/materials.module.ts
+++ apps/api/src/materials/materials.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { MaterialsController } from "./materials.controller";
 
 @Module({
+  imports: [AuthModule],
   controllers: [MaterialsController],
 })
 export class MaterialsModule {}
